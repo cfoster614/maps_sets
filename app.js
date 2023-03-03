06bc289de1ced0083cfd1b75f1a0352853a55a8a
Caity <cjaynefoster@gmail.com>
2023-03-03 16:31:01 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const numbers = new Set([1,1,2,2,3,4]) // {1,2,3,4}
+
+const referee = [...new Set("referee")].join("") // "ref"
+
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+//0: {Array(3) => true}
+//1: {Array(3) => false}
+
+
+const hasDuplicate = arr => new Set(arr).size !== arr.length
+
+function isVowel(char){
+    return "aeiou".includes(char);
+}
+
+function vowelCount(str){
+    const vowelMap = new Map();
+    for(let char of str){
+        let lowerCaseChar = char.toLowerCase();
+        if(isVowel(lowerCaseChar)){
+            vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+        } else {
+            vowelMap.set(lowerCaseChar, 1);
+        }
+    }
+    return vowelMap;
+}
